e8c1b600e5233c4e6e0e9c302b9bc24ee0769f25
Gustavo Garcia <gcastilhos@gmail.com>
2020-05-27 17:47:15 +1000

    Simplified initial data load

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 "use strict";
 
-sessionStorage.block_no = [432, 433, 434, 435, 436, 437];
-sessionStorage.nonce = ["375", "381d", "8a5", "14e6", "a8", "3939"];
-sessionStorage.hash_code = ["000758091afabb677e7be6ee7af085182532b1e8cb61bedc2f27d2240b92c05c", "0006e9aec9a9a95642d13a5d1c5436fd988f1b711f715c149b097506b27db971", "000807f16ee4277f84cded8579704490c405a8de454d979f3b89ebd0a35238e0", "0008aeefe5d86dd2768a1b489ed0c8e307ca857910186f54e8632ce3735e26f5", "00074c8cecdc29a9b8ffd5a81efa0cfbecd495ed22e29bf3e8e281650f43252b", "00033afecbf2625ec63885169d9d0626f61d8d1a494f6306269226c6bf9ed6e9"];
-sessionStorage.previous_hash = ["0005100308e7e0bea95a3e88e4e406c37133f0929c80866bda04bc0bce53a15", "000758091afabb677e7be6ee7af085182532b1e8cb61bedc2f27d2240b92c05c", "0006e9aec9a9a95642d13a5d1c5436fd988f1b711f715c149b097506b27db971", "000807f16ee4277f84cded8579704490c405a8de454d979f3b89ebd0a35238e0", "0008aeefe5d86dd2768a1b489ed0c8e307ca857910186f54e8632ce3735e26f5", "00074c8cecdc29a9b8ffd5a81efa0cfbecd495ed22e29bf3e8e281650f43252b"];
 sessionStorage.eventData = ["Date|Time|EventID|Appl.ID |ActivePWR |ReactivePwr|Voltage|Intensity |HASH\u003e\u003e\u003e 10Mar2015 0:00:00 1201210 9b75a5178a 2.58 0.136 241.97 10.6\u003e\u003e\u003e 10Mar2015 0:01:00 1201211 9b75a5178f 2.552 0.1 241.75 10.4\u003e\u003e\u003e 10Mar2015 0:02:00 1201212 9b75a51791 2.55 0.1 241.64 10.4\u003e\u003e\u003e 10Mar2015 0:03:00 1201213 9b75a5178d 2.55 0.1 241.71 10.4\u003e\u003e\u003e 10Mar2015 0:04:00 1201214 9b75a5178b 2.554 0.1 241.98 10.4\u003e\u003e\u003e 10Mar2015 0:05:00 1201215 9b75a5178f 2.55 0.1 241.83 10.4\u003e\u003e\u003e 10Mar2015 0:06:00 1201216 9b75a51790 2.534 0.09 241.07 10.4\u003e\u003e\u003e 10Mar2015 0:07:00 1201217 9b75a51791 2.484 0.21 241.29 10.2\u003e\u003e\u003e 10Mar2015 0:08:00 1201218 9b75a5178a 2.468 0.32 241.23 10.2\u003e\u003e\u003e 10Mar2015 0:09:00 1201219 9b75a51793 2.48 0.54 242.28 10.2", "Date|Time|EventID|Appl.ID |ActivePWR |ReactivePwr|Voltage|Intensity |HASH\u003e\u003e\u003e 10Mar2015 0:00:00 1201210 9b75a5178a 2.58 0.136 241.97 10.6\u003e\u003e\u003e 10Mar2015 0:01:00 1201211 9b75a5178f 2.552 0.1 241.75 10.4\u003e\u003e\u003e 10Mar2015 0:02:00 1201212 9b75a51791 2.55 0.1 241.64 10.4\u003e\u003e\u003e 10Mar2015 0:03:00 1201213 9b75a5178d 2.55 0.1 241.71 10.4\u003e\u003e\u003e 10Mar2015 0:04:00 1201214 9b75a5178b 2.554 0.1 241.98 10.4\u003e\u003e\u003e 10Mar2015 0:05:00 1201215 9b75a5178f 2.55 0.1 241.83 10.4\u003e\u003e\u003e 10Mar2015 0:06:00 1201216 9b75a51790 2.534 0.09 241.07 10.4\u003e\u003e\u003e 10Mar2015 0:07:00 1201217 9b75a51791 2.484 0.21 241.29 10.2\u003e\u003e\u003e 10Mar2015 0:08:00 1201218 9b75a5178a 2.468 0.32 241.23 10.2\u003e\u003e\u003e 10Mar2015 0:09:00 1201219 9b75a51793 2.48 0.54 242.28 10.2", "Date|Time|EventID|Appl.ID |ActivePWR |ReactivePwr|Voltage|Intensity |HASH\u003e\u003e\u003e 10Mar2015 0:00:00 1201210 9b75a5178a 2.58 0.136 241.97 10.6\u003e\u003e\u003e 10Mar2015 0:01:00 1201211 9b75a5178f 2.552 0.1 241.75 10.4\u003e\u003e\u003e 10Mar2015 0:02:00 1201212 9b75a51791 2.55 0.1 241.64 10.4\u003e\u003e\u003e 10Mar2015 0:03:00 1201213 9b75a5178d 2.55 0.1 241.71 10.4\u003e\u003e\u003e 10Mar2015 0:04:00 1201214 9b75a5178b 2.554 0.1 241.98 10.4\u003e\u003e\u003e 10Mar2015 0:05:00 1201215 9b75a5178f 2.55 0.1 241.83 10.4\u003e\u003e\u003e 10Mar2015 0:06:00 1201216 9b75a51790 2.534 0.09 241.07 10.4\u003e\u003e\u003e 10Mar2015 0:07:00 1201217 9b75a51791 2.484 0.21 241.29 10.2\u003e\u003e\u003e 10Mar2015 0:08:00 1201218 9b75a5178a 2.468 0.32 241.23 10.2\u003e\u003e\u003e 10Mar2015 0:09:00 1201219 9b75a51793 2.48 0.54 242.28 10.2", "Date|Time|EventID|Appl.ID |ActivePWR |ReactivePwr|Voltage|Intensity |HASH\u003e\u003e\u003e 10Mar2015 0:00:00 1201210 9b75a5178a 2.58 0.136 241.97 10.6\u003e\u003e\u003e 10Mar2015 0:01:00 1201211 9b75a5178f 2.552 0.1 241.75 10.4\u003e\u003e\u003e 10Mar2015 0:02:00 1201212 9b75a51791 2.55 0.1 241.64 10.4\u003e\u003e\u003e 10Mar2015 0:03:00 1201213 9b75a5178d 2.55 0.1 241.71 10.4\u003e\u003e\u003e 10Mar2015 0:04:00 1201214 9b75a5178b 2.554 0.1 241.98 10.4\u003e\u003e\u003e 10Mar2015 0:05:00 1201215 9b75a5178f 2.55 0.1 241.83 10.4\u003e\u003e\u003e 10Mar2015 0:06:00 1201216 9b75a51790 2.534 0.09 241.07 10.4\u003e\u003e\u003e 10Mar2015 0:07:00 1201217 9b75a51791 2.484 0.21 241.29 10.2\u003e\u003e\u003e 10Mar2015 0:08:00 1201218 9b75a5178a 2.468 0.32 241.23 10.2\u003e\u003e\u003e 10Mar2015 0:09:00 1201219 9b75a51793 2.48 0.54 242.28 10.2", "Date|Time|EventID|Appl.ID |ActivePWR |ReactivePwr|Voltage|Intensity |HASH\u003e\u003e\u003e 10Mar2015 0:00:00 1201210 9b75a5178a 2.58 0.136 241.97 10.6\u003e\u003e\u003e 10Mar2015 0:01:00 1201211 9b75a5178f 2.552 0.1 241.75 10.4\u003e\u003e\u003e 10Mar2015 0:02:00 1201212 9b75a51791 2.55 0.1 241.64 10.4\u003e\u003e\u003e 10Mar2015 0:03:00 1201213 9b75a5178d 2.55 0.1 241.71 10.4\u003e\u003e\u003e 10Mar2015 0:04:00 1201214 9b75a5178b 2.554 0.1 241.98 10.4\u003e\u003e\u003e 10Mar2015 0:05:00 1201215 9b75a5178f 2.55 0.1 241.83 10.4\u003e\u003e\u003e 10Mar2015 0:06:00 1201216 9b75a51790 2.534 0.09 241.07 10.4\u003e\u003e\u003e 10Mar2015 0:07:00 1201217 9b75a51791 2.484 0.21 241.29 10.2\u003e\u003e\u003e 10Mar2015 0:08:00 1201218 9b75a5178a 2.468 0.32 241.23 10.2\u003e\u003e\u003e 10Mar2015 0:09:00 1201219 9b75a51793 2.48 0.54 242.28 10.2", "Date|Time|EventID|Appl.ID |ActivePWR |ReactivePwr|Voltage|Intensity |HASH\u003e\u003e\u003e 10Mar2015 0:00:00 1201210 9b75a5178a 2.58 0.136 241.97 10.6\u003e\u003e\u003e 10Mar2015 0:01:00 1201211 9b75a5178f 2.552 0.1 241.75 10.4\u003e\u003e\u003e 10Mar2015 0:02:00 1201212 9b75a51791 2.55 0.1 241.64 10.4\u003e\u003e\u003e 10Mar2015 0:03:00 1201213 9b75a5178d 2.55 0.1 241.71 10.4\u003e\u003e\u003e 10Mar2015 0:04:00 1201214 9b75a5178b 2.554 0.1 241.98 10.4\u003e\u003e\u003e 10Mar2015 0:05:00 1201215 9b75a5178f 2.55 0.1 241.83 10.4\u003e\u003e\u003e 10Mar2015 0:06:00 1201216 9b75a51790 2.534 0.09 241.07 10.4\u003e\u003e\u003e 10Mar2015 0:07:00 1201217 9b75a51791 2.484 0.21 241.29 10.2\u003e\u003e\u003e 10Mar2015 0:08:00 1201218 9b75a5178a 2.468 0.32 241.23 10.2\u003e\u003e\u003e 10Mar2015 0:09:00 1201219 9b75a51793 2.48 0.54 242.28 10.2"];
-sessionStorage.size = 6;
 
 (function() {
 
@@ -16,7 +11,7 @@ sessionStorage.size = 6;
         return false;
     };
 
-    var _getHash = function(id) {
+    var _encode = function(id) {
         var nonce = -1;
         var hash_code = undefined;
         while (!_valid_hash(hash_code)) {
@@ -24,38 +19,38 @@ sessionStorage.size = 6;
             var text_to_encode = app.previous_hash[id] + app.eventData[id] + nonce;
             hash_code = sha256(text_to_encode);
         }
-        app.$set(app.nonce, id, nonce);
-        app.$set(app.hash, id, hash_code);
-        app.$set(app.active, id, (app.original_hash[id] != app.hash[id]));
-        if (id < app.size - 1) {
-            app.$set(app.previous_hash, id + 1, app.hash[id]);
-            _getHash(id + 1);
-        }
-    };
-
-    var _blockNoFormat = function(values) {
-        var size = values.length;
-        var new_values = new Array(size);
-        for (var i = 0; i < size; i++) {
-            new_values[i] = ("000" + values[i]).slice(-6);
-        }   
-        return new_values; 
+        return {"nonce": nonce, "hash": hash_code};
     };
 
     var app = new Vue({
         el:'#app',
         data: {
-            block_no: _blockNoFormat(sessionStorage.block_no.split(",")),
-            hash: sessionStorage.hash_code.split(","),
-            original_hash: sessionStorage.hash_code.split(","),
-            previous_hash: sessionStorage.previous_hash.split(","),
-            nonce: sessionStorage.nonce.split(","),
+            block_no: ["000432", "000433", "000434", "000435", "000436", "000437"],
+            hash: ['', '', '', '', '', ''],
+            original_hash: ['', '', '', '', '', ''],
+            previous_hash: ["0005100308e7e0bea95a3e88e4e406c37133f0929c80866bda04bc0bce53a14", '', '', '', '', ''],
+            nonce: ['', '', '', '', '', ''],
             eventData: sessionStorage.eventData.split(","),
-            active: new Array(sessionStorage.size).fill(false),
-            size: sessionStorage.size
+            active: new Array(6).fill(false),
+            size: 6
         },
         methods: {
-            getHash: _getHash
+            getHash: function(id) {
+                var data = _encode(id);
+                app.$set(app.nonce, id, data.nonce);
+                app.$set(app.hash, id, data.hash);
+                if (app.original_hash[id] === '') {
+                    app.$set(app.original_hash, id, app.hash[id]);
+                } else {
+                    app.$set(app.active, id, (app.original_hash[id] != app.hash[id]));
+                }
+                if (id < app.size - 1) {
+                    app.$set(app.previous_hash, id + 1, app.hash[id]);
+                    app.getHash(id + 1);
+                }
+            }
         }
     });
+
+    app.getHash(0);
 })();
